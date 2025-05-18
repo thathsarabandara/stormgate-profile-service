6128ce522e42937126ccfe455d33d5ab52338674
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-18 15:37:08 +0530

    add profile custom fields entity

---
+++ src/main/java/thathsarabandara/profile_service/model/ProfileCustomField.java
@@ -0,0 +1,39 @@
+package thathsarabandara.profile_service.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "profile_custom_fields")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileCustomField {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "profile_id", nullable = false)
+    private Profile profile;
+
+    @Column(name = "field_name", length = 255, nullable = false)
+    private String fieldName;
+
+    @Lob
+    @Column(name = "field_value", nullable = false, columnDefinition = "TEXT")
+    private String fieldValue;
+}
--- src/main/java/thathsarabandara/profile_service/model/ProfileDetail.java
+++ src/main/java/thathsarabandara/profile_service/model/ProfileDetail.java
@@ -27,7 +27,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProfileDetails {
+public class ProfileDetail {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
