de3da9d12b7e494abe25ab0608e20fd06d494f0c
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-20 10:31:14 +0530

    Profile Service, Profile controller

--- src/main/java/thathsarabandara/profile_service/controller/ProfileController.java
+++ src/main/java/thathsarabandara/profile_service/controller/ProfileController.java
@@ -1,10 +1,25 @@
 package thathsarabandara.profile_service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import thathsarabandara.profile_service.dtos.ProfileRequest;
+import thathsarabandara.profile_service.dtos.ProfileResponse;
+import thathsarabandara.profile_service.service.ProfileService;
 
 @RestController
 @RequestMapping("/api/v1/profile")
 public class ProfileController {
-    
+    @Autowired ProfileService profileService;
+    @PostMapping("/")
+    public ResponseEntity<ProfileResponse> createProfile(
+        @RequestHeader(value = "Tenant-ID", required = true) String Tenantid,
+        @ModelAttribute ProfileRequest request) {
+        return profileService.create(Tenantid, request);
+    }
 }
