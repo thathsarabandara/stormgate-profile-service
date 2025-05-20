3be0fa654f41ae1c30bb2f97f281f143df0b312a
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-05-20 06:01:34 +0530

    changed file due to checkstyle errors

--- src/main/java/thathsarabandara/profile_service/repository/ProfileCustomFeildRepository.java
+++ src/main/java/thathsarabandara/profile_service/repository/ProfileCustomFeildRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import thathsarabandara.profile_service.model.Profile;
 import thathsarabandara.profile_service.model.ProfileCustomField;
 
-public interface ProfileCustomFeildRepository extends JpaRepository<ProfileCustomField, Long>{
+public interface ProfileCustomFeildRepository extends JpaRepository<ProfileCustomField, Long> {
     Optional<ProfileCustomField> findByProfile(Profile profile);
 }
